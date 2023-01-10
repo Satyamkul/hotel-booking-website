a275f65d4221558ec1f2578e6fb7f3ffcaf81894
Satyamkul <satyamkul2002@gmail.com>
2023-01-10 11:47:49 +0530

    spinner js removed

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
     // Initiate the wowjs
     new WOW().init();
-    
     
     // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
