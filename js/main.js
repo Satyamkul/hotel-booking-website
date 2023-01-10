cd0f5fd284a4365371e9418801bc8f46c12c3005
Satyamkul <satyamkul2002@gmail.com>
2023-01-10 18:33:50 +0530

    testimonial updated

--- js/main.js
+++ js/main.js
@@ -3,34 +3,34 @@
 
     // Initiate the wowjs
     new WOW().init();
-    
+
     // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
     const $dropdownToggle = $(".dropdown-toggle");
     const $dropdownMenu = $(".dropdown-menu");
     const showClass = "show";
-    
-    $(window).on("load resize", function() {
+
+    $(window).on("load resize", function () {
         if (this.matchMedia("(min-width: 992px)").matches) {
             $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
+                function () {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "true");
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function () {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "false");
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
             );
         } else {
             $dropdown.off("mouseenter mouseleave");
         }
     });
-    
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -40,7 +40,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -71,26 +71,45 @@
 
 
     // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        dots: false,
+    //     $(".testimonial-carousel").owlCarousel({
+    //         autoplay: true,
+    //         smartSpeed: 1000,
+    //         margin: 25,
+    //         dots: false,
+    //         loop: true,
+    //         nav : true,
+    //         navText : [
+    //             '<i class="bi bi-arrow-left"></i>',
+    //             '<i class="bi bi-arrow-right"></i>'
+    //         ],
+    //         responsive: {
+    //             0:{
+    //                 items:1
+    //             },
+    //             768:{
+    //                 items:2
+    //             }
+    //         }
+    //     });
+
+    // 
+
+    $(".testimonials").owlCarousel({
+        margin: 20,
         loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
+        autoplay: true,
+        autoplayTimeout: 8000,
+        autoplayHoverPause: true,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
             }
         }
     });
-    
 })(jQuery);
-
