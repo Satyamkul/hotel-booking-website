bc9ed74b5cd3a5c3acc487fa41eadb1632d8fbe7
Satyamkul <satyamkul2002@gmail.com>
2023-01-11 21:12:30 +0530

    update

--- js/main.js
+++ js/main.js
@@ -69,31 +69,6 @@
         })
     });
 
-
-    // Testimonials carousel
-    //     $(".testimonial-carousel").owlCarousel({
-    //         autoplay: true,
-    //         smartSpeed: 1000,
-    //         margin: 25,
-    //         dots: false,
-    //         loop: true,
-    //         nav : true,
-    //         navText : [
-    //             '<i class="bi bi-arrow-left"></i>',
-    //             '<i class="bi bi-arrow-right"></i>'
-    //         ],
-    //         responsive: {
-    //             0:{
-    //                 items:1
-    //             },
-    //             768:{
-    //                 items:2
-    //             }
-    //         }
-    //     });
-
-    // 
-
     $(".testimonials").owlCarousel({
         margin: 20,
         loop: true,
